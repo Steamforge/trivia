751aba89cd4b411d98760f832aca94c0c50fbb1e
Steamforge <joshstudley@gmail.com>
2018-08-09 13:41:13 -0700

    move questions to own api

--- src/server/routes/api-routes.js
+++ src/server/routes/api-routes.js
@@ -6,6 +6,21 @@ const db = require('../models');
 
 module.exports = (app) => {
 
+    //get questions
+    app.get('/api/questions/:dataType?', (req, res) => {
+        if (req.params.dataType === 'mock') {
+            res.json(mockData);
+        } else {
+            request(dataUrl, (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    res.json(JSON.parse(body));
+                } else {
+                    res.status(500).json({ error: 500, message: 'There was a problem retieving questions. Please try again.' });
+                }
+            });
+        }
+    });
+
     //get Game or Player
     app.get('/api/:model/:id?', (req, res) => {
         let Model;
@@ -26,31 +41,14 @@ module.exports = (app) => {
                 res.json(dbGames);
             });
         } else {
-            res.status(404).json({ error: 404, message: `${req.params.model} not found in database` });
+            res.status(404).json({ error: 404, message: `/api/${req.params.model} does not exist` });
         }
     });
 
     //create new game
-    app.post('/api/game/new/:dataType?', (req, res) => {
-        const gameObject = req.body;
-        if (req.params.dataType === 'mock') {
-            //use mock data
-            gameObject.gameData = mockData;
-            db.Game.create(gameObject).then((dbGame) => {
-                res.json(dbGame);
-            });
-        } else {
-            //use api data
-            request(dataUrl, (error, response, body) => {
-                if (!error && response.statusCode === 200) {
-                    gameObject.gameData = JSON.parse(body);
-                    db.Game.create(gameObject).then((dbGame) => {
-                        res.json(dbGame);
-                    });
-                } else {
-                    res.status(500).json({ error: 500, message: 'Problem creating game. PLease try again.' });
-                }
-            });
-        }
+    app.post('/api/game/new', (req, res) => {
+        db.Game.create(req.body).then((dbGame) => {
+            res.json(dbGame);
+        });
     });
 };
